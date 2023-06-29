6a69abd4912325ab24e0a10ef5fed50c452625f3
Anna Krasilnikova <akr6363@mail.ru>
2023-06-30 01:48:19 +0300

    the Prettier is connected the redux-toolkit library is connected the reducers have been translated into syntax redux-toolkit

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,57 +1,44 @@
-import {Dispatch} from 'redux'
-import {authAPI} from '../api/todolists-api'
-import {setIsLoggedInAC} from '../features/Login/auth-reducer'
+import { authAPI } from "../api/todolists-api";
+import { authActions } from "features/Login/auth-reducer";
+import { createSlice, current, PayloadAction } from "@reduxjs/toolkit";
+import { AppThunk } from "app/store";
 
-const initialState: InitialStateType = {
-    status: 'idle',
-    error: null,
-    isInitialized: false
-}
+const initialState = {
+  status: "idle" as RequestStatusType,
+  error: null as string | null,
+  isInitialized: false,
+};
 
-export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-    switch (action.type) {
-        case 'APP/SET-STATUS':
-            return {...state, status: action.status}
-        case 'APP/SET-ERROR':
-            return {...state, error: action.error}
-        case 'APP/SET-IS-INITIALIED':
-            return {...state, isInitialized: action.value}
-        default:
-            return {...state}
+export type AppInitialStateType = typeof initialState;
+
+const slice = createSlice({
+  name: "app",
+  initialState,
+  reducers: {
+    setAppErrorAC(state, action: PayloadAction<{ error: string | null }>) {
+      state.error = action.payload.error;
+    },
+    setAppStatusAC(state, action: PayloadAction<{ status: RequestStatusType }>) {
+      console.log(current(state));
+      state.status = action.payload.status;
+    },
+    setAppInitializedAC(state, action: PayloadAction<{ isInitialized: boolean }>) {
+      state.isInitialized = action.payload.isInitialized;
+    },
+  },
+});
+
+export const appReducer = slice.reducer;
+export const appActions = slice.actions;
+
+export type RequestStatusType = "idle" | "loading" | "succeeded" | "failed";
+
+export const initializeAppTC = (): AppThunk => (dispatch) => {
+  authAPI.me().then((res) => {
+    if (res.data.resultCode === 0) {
+      dispatch(authActions.setIsLoggedInAC({ isLoggedIn: true }));
+    } else {
     }
-}
-
-export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
-export type InitialStateType = {
-    // происходит ли сейчас взаимодействие с сервером
-    status: RequestStatusType
-    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
-    error: string | null
-    // true когда приложение проинициализировалось (проверили юзера, настройки получили и т.д.)
-    isInitialized: boolean
-}
-
-export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
-export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
-export const setAppInitializedAC = (value: boolean) => ({type: 'APP/SET-IS-INITIALIED', value} as const)
-
-export const initializeAppTC = () => (dispatch: Dispatch) => {
-    authAPI.me().then(res => {
-        if (res.data.resultCode === 0) {
-            dispatch(setIsLoggedInAC(true));
-        } else {
-
-        }
-
-        dispatch(setAppInitializedAC(true));
-    })
-}
-
-export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
-export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
-
-
-type ActionsType =
-    | SetAppErrorActionType
-    | SetAppStatusActionType
-    | ReturnType<typeof setAppInitializedAC>
+    dispatch(appActions.setAppInitializedAC({ isInitialized: true }));
+  });
+};
--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,17 +1,17 @@
-import {setAppErrorAC, SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from '../app/app-reducer'
-import {ResponseType} from '../api/todolists-api'
-import {Dispatch} from 'redux'
+import { ResponseType } from "../api/todolists-api";
+import { Dispatch } from "redux";
+import { appActions } from "app/app-reducer";
 
-export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
-    if (data.messages.length) {
-        dispatch(setAppErrorAC(data.messages[0]))
-    } else {
-        dispatch(setAppErrorAC('Some error occurred'))
-    }
-    dispatch(setAppStatusAC('failed'))
-}
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
+  if (data.messages.length) {
+    dispatch(appActions.setAppErrorAC({ error: data.messages[0] }));
+  } else {
+    dispatch(appActions.setAppErrorAC({ error: "Some error occurred" }));
+  }
+  dispatch(appActions.setAppStatusAC({ status: "failed" }));
+};
 
-export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
-    dispatch(setAppErrorAC(error.message ? error.message : 'Some error occurred'))
-    dispatch(setAppStatusAC('failed'))
-}
+export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch) => {
+  dispatch(appActions.setAppErrorAC({ error: error.message ? error.message : "Some error occurred" }));
+  dispatch(appActions.setAppStatusAC({ status: "failed" }));
+};
